10578dbab5d60b940d1a4fdc382c94cf47d8c513
Mark Haskamp <mark.haskamp@pinnsg.com>
2014-10-31 19:00:56 -0400

    lots of changes

--- lib/world.ex
+++ lib/world.ex
@@ -1,41 +1,95 @@
 
 defmodule World do
 
-  def init do
-    Agent.start_link fn -> [] end, name: __MODULE__
-  end
+  use GenServer
 
-  def init(cells) do
-    Agent.start_link fn -> cells end, name: __MODULE__
-  end
+  def start_link,               do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  def start_link(initial_list), do: GenServer.start_link(__MODULE__, initial_list, name: __MODULE__)
 
-  def add_cell(cell) do
-    Agent.update(__MODULE__, fn list -> [cell | list] end)
-  end
+  # public api
+  def add_cell(cell), do: GenServer.cast(__MODULE__, {:add_cell, cell})
+  def empty?,         do: GenServer.call(__MODULE__, :empty?)
 
-  def inspect do
-    Agent.get(__MODULE__, fn list -> list end)
-  end
-
-
-  def empty? do
-    my_list = Agent.get(__MODULE__, fn list -> list end)
-    Enum.count(my_list) == 0
-  end
-
-  def get_neighbors_for cell do
-    x = elem(cell,0)
-    y = elem(cell,1)
-
-    for i <- x-1..x+1, i>= 0 do
-      for j <- y-1..y+1, j>=0 and (i != x or j != y) do
-        {i,j}
+  def get_neighbors_for(cell) do
+    x = elem cell,0
+    y = elem cell,1
+    for i <- x-1..x+1, i >= 0 do
+      for j <- y-1..y+1, j >= 0  do
+        {i, j}
       end
     end
 
     |>
     List.flatten
+
+    |>
+    Enum.filter(&(&1 != {x,y}))
   end
+
+  def get_alive_neighbors_for(cell) do
+    alive_cells = GenServer.call(__MODULE__, :alive_cells)
+
+    get_neighbors_for(cell)
+    |>
+    Enum.filter(&(Enum.member?(alive_cells, &1)))
+  end
+
+  def get_dead_neighbors_for(cell) do
+    alive_cells = GenServer.call(__MODULE__, :alive_cells)
+
+    get_neighbors_for(cell)
+    |>
+    Enum.filter(&(Enum.member?(alive_cells, &1) == false))
+  end
+
+  def react_to_tick do
+    alive_cells = GenServer.call(__MODULE__, :alive_cells)
+    still_alive = get_cells_to_remain_alive(alive_cells)
+    zombie_cells = get_cells_to_come_alive(alive_cells)
+
+    Enum.concat(still_alive, zombie_cells)
+  end
+
+  def get_cells_to_remain_alive(alive_cells) do
+    Enum.filter(alive_cells, fn(t) ->
+      count = Enum.count(get_alive_neighbors_for(t))
+      count > 1 and count < 4
+    end)
+  end
+
+  def get_cells_to_come_alive(alive_cells) do
+    Enum.map(alive_cells, &(get_dead_neighbors_for(&1)))
+    |>
+    List.flatten
+    |>
+    Enum.filter(fn(t) ->
+      Enum.count(get_alive_neighbors_for(t)) == 3
+    end)
+    |>
+    uniq
+  end
+
+  def uniq(list) do
+    Enum.into(list, HashSet.new)
+    |>
+    Enum.to_list
+  end
+
+
+
+  # GenServer implementation
+  def handle_cast({:add_cell, cell}, state) do
+    {:noreply, [cell | state]}
+  end
+
+  def handle_call(:empty?, _from, alive_cells) do
+    {:reply, Enum.count(alive_cells) == 0, alive_cells}
+  end
+
+  def handle_call(:alive_cells, _from, alive_cells) do
+    {:reply, alive_cells, alive_cells}
+  end
+
 
 end
 
--- test/game_of_life_test.exs
+++ test/game_of_life_test.exs
@@ -1,8 +1,63 @@
 defmodule GameOfLifeTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "a new World, started with no parameter, has no alive cells" do
+    World.start_link
+    assert World.empty?
   end
 
+  test "a new World can be started with a initial list of alive cells" do
+    World.start_link [{1,1}]
+    assert World.empty? == false
+  end
+
+  test "add_cell adds cell" do
+    World.start_link
+    assert World.empty?
+    World.add_cell({1,1})
+    assert World.empty? == false
+  end
+
+  test "get neighbors for {0,0}" do
+    neighbors = World.get_neighbors_for {0,0}
+    assert Enum.sort(neighbors) == Enum.sort([{1,0}, {0,1}, {1,1}])
+  end
+
+  test "get alive neighbors for a cell" do
+    World.start_link [{1,1}, {2,1}, {3,1}]
+    assert World.get_alive_neighbors_for({1,1}) == [{2,1}]
+    assert World.get_alive_neighbors_for({2,1}) == [{1,1}, {3,1}]
+    assert World.get_alive_neighbors_for({3,1}) == [{2,1}]
+  end
+
+  test "get dead neighbors for a cell" do
+    World.start_link [{1,1}, {2,1}, {3,1}, {1,2}]
+    assert Enum.sort(World.get_dead_neighbors_for({1,1})) == Enum.sort([{0, 0}, {0, 1}, {0, 2}, {1, 0}, {2, 0}, {2, 2}])
+    assert Enum.sort(World.get_dead_neighbors_for({2,1})) == Enum.sort([{1, 0}, {2, 0}, {2, 2}, {3, 0}, {3, 2}])
+    assert Enum.sort(World.get_dead_neighbors_for({3,1})) == Enum.sort([{2, 0}, {2, 2}, {3, 0}, {3, 2}, {4, 0}, {4, 1}, {4, 2}])
+  end
+
+  test "get cells to remain alive" do
+    World.start_link [{1,1}, {2,1}, {3,1}]
+    assert World.get_cells_to_remain_alive([{1,1}, {2,1}, {3,1}]) == [{2,1}]
+  end
+
+  test "react to tick" do
+    World.start_link [{1,1}, {2,1}, {3,1}, {1,2}, {8,8}]
+    new_cells = World.react_to_tick
+    assert Enum.sort(new_cells) == Enum.sort([{1, 1}, {1, 2}, {2, 0}, {2, 1}])
+  end
+
+  test "also react to tick" do
+    World.start_link [{1,1}, {2,1}, {3,1}, {1,2}, {3,0}, {3,2}]
+    new_cells = World.react_to_tick
+    assert Enum.count(new_cells) == 6
+    assert Enum.member?(new_cells, {3,0}) == true
+    assert Enum.member?(new_cells, {1,1}) == true
+    assert Enum.member?(new_cells, {3,1}) == true
+    assert Enum.member?(new_cells, {4,1}) == true
+    assert Enum.member?(new_cells, {1,2}) == true
+    assert Enum.member?(new_cells, {3,2}) == true
+    assert Enum.member?(new_cells, {2,1}) == false
+  end
 end
